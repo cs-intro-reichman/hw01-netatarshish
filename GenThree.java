6d0ba6e7d1b403f7db4709f5d70e9dd5025f2bc5
Neta Tarshish <neta.tarshish@runi.ac.il>
2023-12-20 14:17:03 +0200

    add AddTwo file

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,22 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+			int minimum = Integer.parseInt(args[0]);
+		int maximum = Integer.parseInt(args[1]);
+		int minimumGenerated = minimum;
+		int i = 0;
+		while (i<3)	{
+			int num = (int)(Math.random()*maximum);
+			if(num>=minimum&&num<maximum){ //checks if the current number genarated is the minimal one
+				System.out.println(num);
+				if(i==0){
+					minimumGenerated = num;
+				}
+				if(num<minimumGenerated){
+					minimumGenerated = num;
+				}
+				i=i+1;
+	}}
+	System.out.println("the minimal generated number was "+minimumGenerated);	
 	}
 }
